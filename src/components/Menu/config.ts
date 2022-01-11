304045bd04c26e837800dbb6e2dca44c88958e47
Stripey Zebra <85252150+toadgames@users.noreply.github.com>
2022-01-11 20:30:33 +0200

    Updated the links

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -13,6 +13,10 @@ const config: MenuEntry[] = [
         label: "MoonRiver",
         href: "https://movr.padswap.exchange",
       },
+      {
+        label: "MoonBeam",
+        href: "https://glmr.padswap.exchange",
+      },
     ],
     // calloutClass: "rainbow",
   },
@@ -32,63 +36,20 @@ const config: MenuEntry[] = [
     ],
   },
   {
-    label: "LaunchPad",
-    icon: "VaultIcon",
-    href: "https://movr-dapps.padswap.exchange/launchpad",
-  },
-  {
-    label: "Vault",
-    icon: "VaultIcon",
-    href: "https://movr-dapps.padswap.exchange/vault",
-  },
-  {
-    label: 'Liquidity Farm',
-    icon: 'PoolIcon',
-    href: 'https://movr-dapps.padswap.exchange/lpfarms',
-  },
-  {
     label: 'Farms',
     icon: 'ChartIcon',
-    href: 'https://movr-dapps.padswap.exchange',
+    href: 'https://dapps.padswap.exchange/farms',
   },
   {
-    label: 'Stats',
+    label: 'Bridge',
     icon: 'ChartIcon',
-    href: 'https://movr-info.padswap.exchange',
+    href: 'https://dapps.padswap.exchange/bridge',
   },
   {
-    label: "TOAD",
-    icon: "ToadLogo",
-    items: [
-      {
-        label: "Farms",
-        href: "https://toad.network/#/why_choose",
-      },
-      {
-        label: "LP Farms",
-        href: "https://toad.network/lpfarming.html",
-      },
-      {
-        label: "Church",
-        href: "https://toadnetwork.church",
-      },
-    ],
-    // calloutClass: "rainbow",
-  },
-  {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: "Github",
-        href: "https://github.com/ToadNetwork",
-      },
-      {
-        label: "Medium",
-        href: "https://toadytoad.medium.com/",
-      },
-    ],
-  },
+    label: 'Vault',
+    icon: 'ChartIcon',
+    href: 'https://dapps.padswap.exchange/vault',
+  }
 ]
 
 export default config
